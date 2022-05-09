4c5809d622732f7d3a3c43a9cd8a901d82e2e9d1
ahmed9190 <hamadawaled11@gmail.com>
2022-05-09 21:18:58 +0200

    update version

--- routes/api.php
+++ routes/api.php
@@ -24,8 +24,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-const version = 1.9;
-const apkUrl = "https://www.mediafire.com/file/j3rb8i9zzv0ahft/app-release-10.apk/file";
+const version = 2.0;
+const apkUrl = "https://www.mediafire.com/file/37ltmuop2174x49/app-release-11.apk/file";
 
 date_default_timezone_set("Asia/Riyadh");
 
